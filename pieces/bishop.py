95b37d7d3b23e1ebdbefedc50dcb84216128e9d4
Erik Klockare <eklockare@gmail.com>
2015-06-29 09:02:55 +0200

    implemented bishop

---
+++ pieces/bishop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from pieces.piece import Piece
+from board_parts import white, black
+from directions import move_directions_bishop, DirectionResult
+import board_parts as bps
+
+
+class Bishop(Piece):
+    def __init__(self, chess_coord, colour):
+        if colour is white:
+            Piece.__init__(self, chess_coord, white, 'B', '♗', move_directions_bishop())
+        else:
+            Piece.__init__(self, chess_coord, black, 'B', '♝', move_directions_bishop())
---
+++ tests/test_bishop.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import test
+import board_parts
+from board_parts import GridCoord, ChessCoord, black, white
+from pieces.bishop import Bishop
+import util
+import directions
+
+
+class BishopTests(unittest.TestCase):
+    def setUp(self):
+        chess_coord_white = ChessCoord('F', '8')
+        self.bishop_white = Bishop(chess_coord_white, white)
+        chess_coord_black = ChessCoord('E', '3')
+        self.bishop_black = Bishop(chess_coord_black, black)
+
+    def test_constructor_white(self):
+        self.failUnless(self.bishop_white.letter == 'B')
+        self.failUnless(self.bishop_white.chess_coord == ChessCoord('F', '8'))
+        self.failUnless(self.bishop_white.grid_coord == GridCoord(5, 7))
+        self.failUnless(self.bishop_white.colour == white)
+        self.failUnless(self.bishop_white.symbol == '♗')
+        self.failUnless(util.compare_lists(self.bishop_white.move_directions,
+                                           directions.move_directions_bishop()))
+
+    def test_constructor_black(self):
+        self.failUnless(self.bishop_black.letter == 'B')
+        self.failUnless(self.bishop_black.chess_coord == ChessCoord('E', '3'))
+        self.failUnless(self.bishop_black.grid_coord == GridCoord(4, 2))
+        self.failUnless(self.bishop_black.colour == black)
+        self.failUnless(self.bishop_black.symbol == '♝')
+        self.failUnless(util.compare_lists(self.bishop_black.move_directions,
+                                           directions.move_directions_bishop()))
+
+    def test_white_bishop_allowed_to_move_south3_west3(self):
+        pieces = []
+        self.failUnless(self.bishop_white.is_valid_move(pieces,
+                                                        ChessCoord('C', '5')))
+
+    def test_black_bishop_allowed_to_move_north4_west4(self):
+            pieces = []
+            self.failUnless(self.bishop_black.is_valid_move(pieces,
+                                                            ChessCoord('A', '7')))
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
